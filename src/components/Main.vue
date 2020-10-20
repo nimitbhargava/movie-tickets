938c851119d990ca495f2533b0e3703196ac6110
Nimit Bhargava <nimitbhargava@gmail.com>
2020-10-20 11:51:07 +0530

    first commit

---
+++ src/components/Main.vue
@@ -0,0 +1,27 @@
+<template>
+    <b-container class="bv-example-row">
+        <b-row>
+            <b-col>1 of 3</b-col>
+            <b-col>2 of {{ this.count }} 3</b-col>
+            <b-col>1 of 3</b-col>
+        </b-row>
+    </b-container>
+</template>
+
+<script>
+import { mapState } from 'vuex';
+
+export default {
+    name: 'Main',
+    props: {},
+    computed: {
+        ...mapState({
+            count: state => state.count
+        })
+    }
+}
+</script>
+
+<style scoped>
+
+</style>
--- src/main.js
+++ src/main.js
@@ -1,8 +1,27 @@
 import Vue from 'vue'
 import App from './App.vue'
+import { BootstrapVue, IconsPlugin } from 'bootstrap-vue'
+import VueRouter from 'vue-router'
+
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
+
+Vue.use(BootstrapVue)
+Vue.use(IconsPlugin)
+Vue.use(VueRouter)
 
 Vue.config.productionTip = false
 
+import { store } from './store'
+
+import Main from './components/Main'
+
+const router = new VueRouter({
+  routes: [{ path: '/', component: Main}]
+})
+
 new Vue({
+  store,
+  router,
   render: h => h(App),
 }).$mount('#app')
